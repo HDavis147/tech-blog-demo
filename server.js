8b63fb0aee73e6f6153bde60dd0fad2d44f14332
Houston Davis <133936381+HDavis147@users.noreply.github.com>
2023-09-11 16:22:00 -0500

    init tools in server js, add needed files, create main handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const api = require('./controllers/index');
+const helpers = require('./utils/helpers');
 
 // Sets port to required value for Heroku, or 3001
 const PORT = process.env.PORT || 3001;
@@ -9,11 +13,18 @@ const PORT = process.env.PORT || 3001;
 // Creates a new instance of Express
 const app = express();
 
+const hbs = exphbs.create({ helpers });
+
+// Init Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // Express middleware to handle json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
+
+
 
 app.use('/api', api);
 
