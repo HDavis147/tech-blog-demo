29c69e56d36741d076d421ac61f9a49eb38e2845
Houston Davis <133936381+HDavis147@users.noreply.github.com>
2023-09-11 21:03:58 -0500

    add missing routes const

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
